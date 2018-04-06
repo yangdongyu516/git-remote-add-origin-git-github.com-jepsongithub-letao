e626337e61c8a432db2751182f1146ae6a1dc2dc
yang <89640090@qq.com>
2018-04-06 20:10:22 +0800

    登陆功能完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -18,6 +18,14 @@ $(function(){
           notEmpty:{
             //为空时的提示信息
             message:"用户名不能为空"
+          },
+          stringLength: {
+            min: 2,
+            max: 6,
+            message: "用户名长度必须是2-6位"
+          },
+          callback:{
+            message:"用户名不存在"
           }
         }
       },
@@ -33,9 +41,43 @@ $(function(){
             min:6,
             max:12,
             message:"密码长度必须是6-12位"
+          },
+          callback:{
+            message:"密码错误"
           }
         }
       }
     }
+  });
+
+  $("#form").on('success.form.bv', function (e) {
+    var Formdata= $(this).serialize();
+    e.preventDefault();
+    $.ajax({
+      data:Formdata,
+      type:'post',
+      url: "/employee/employeeLogin",
+      dataType:'json',
+      success:function (info) {
+        console.log(info)
+        var validator = $("#form").data('bootstrapValidator'); //获取validator 实例
+        
+        if(info.success){
+          location.href="index.html"
+        }
+        if (info.error===1001){
+          validator.updateStatus("password", "INVALID", "callback")
+        }
+        if(info.error===1000){
+          validator.updateStatus('username', "INVALID","callback")
+        }
+      }
+    })
+  
+  });
+
+  //重置功能
+  $('[type="reset"]').click(function(){
+    $("#form").data('bootstrapValidator').resetForm(true);
   })
 })
